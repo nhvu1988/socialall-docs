781471ac8d8c987e7e8cb691e10ac4338bc39dd5
ThuyVy <dntvy@sandklock.com>
2015-05-14 16:45:10 +0700

    Update css and js for image click event

--- docs/js/soclall.js
+++ docs/js/soclall.js
@@ -1,16 +1,5 @@
-$(document).ready(function() {
-    $('.rst-content img').on('click', function(){
-        if($(this).hasClass('original-size')) {
-            $(this).removeClass('original-size').removeAttr('style');
-        } else {
-            var orW = 0;
-            $("<img/>").attr("src", $(this).attr("src")).load(function() {
-                orW = this.width;
-            });
-            
-            if(orW) {
-                $(this).addClass('original-size').css("width", orW + "px");
-            }
-        }
+$(document).ready(function() {   
+    $('.rst-content img').addClass('small-size').on('click', function(){
+        $(this).toggleClass('big-size');
     });
 });
